b0e65d6e02a81b7cc558633e999cfd62072e194f
Daemon9990 <meruguvasu963@gmail.com>
2024-07-29 15:32:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -51,7 +51,7 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Orangepeel:applelayer@cluster0.0wwtjws.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://parcel:malliga@cluster0.xoax6ur.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
@@ -72,7 +72,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 # Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/Glassmatess_Files')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/GlassmatessBackup')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+uOR17RXDnjk1ZTRl')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/GlassmatessBackup')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/GlassmatessSupportBot') # Support Chat Link Without https:// or @
 
